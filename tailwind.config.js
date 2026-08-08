44f92189d9767959b8b6a55bc510348836df50b0
ANIKET SARAF <aniketsaraf2306@gmail.com>
2026-08-08 15:20:46 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Scan all React/JS/TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
